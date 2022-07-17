e4562d551a49ed3a9e47afbd7cf3a753d9fbdc6d
Evgeniy Shadrin <e.shadrin@aristos.pw>
2022-07-17 17:20:20 +0300

    Добавление компонента BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- eslint-disable-next-line vuejs-accessibility/label-has-for -->
+  <label>{{ label }}</label>
+  <input
+    v-bind="$attrs"
+    :value="modelValue"
+    :placeholder="label"
+    @input="$emit('update:modelValue', $event.target.value)"
+    class="field"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+};
+</script>
